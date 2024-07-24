f6ba57e0b3d32ea92a02b13bfdc214167414fc1e
Heloise Lui <helolui27@gmail.com>
2024-07-24 23:02:04 +0100

    Removed unused packages using depcheck

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/components/Main/MainApp.jsx',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
